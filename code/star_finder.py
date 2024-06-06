294f6352fc320956e357eafb78292d63eb379b4a
toto1105 <ttbjones2004@gmail.com>
2024-06-06 11:56:23 +0200

    Update star_finder.py

--- code/star_finder.py
+++ code/star_finder.py
@@ -30,34 +30,51 @@ import numpy as np
 from matplotlib.colors import LogNorm
 from astropy.stats import sigma_clipped_stats
 from photutils.detection import DAOStarFinder
+from photutils.aperture import CircularAperture
+from photutils.detection import find_peaks
 
-#zet de locatie van member data hier:
+#zet de locatie van member data hier (het zit in hetzelfde mapje dus hoeft niet verandert te worden):
 image_file = get_pkg_data_filename('test.fits')
 hdu = fits.open(image_file)[0]
 wcs = WCS(hdu.header)
 mean, median, std = sigma_clipped_stats(hdu.data, sigma=3.0)
 print('full img stats', mean, median, std)
 
-""" plt.subplot(133, projection=wcs)
-section1 = hdu.data[0:4500, 3000:4500]
-plt.imshow(section1, origin='lower', cmap='Greys', vmin=mean, vmax=mean+5*std)  """
+#hiermee kan je een deel van de data nemen ipv de hele afbeelding
+section1 = hdu.data[0:3900,35:4500]
 
 
-#plt.subplot(131, projection=wcs) 
-plt.subplot(projection=wcs) 
-plt.imshow(hdu.data, origin='lower', cmap='Greys', vmin=np.mean(hdu.data), vmax=0.0005*np.max(hdu.data)+np.mean(hdu.data)) 
+plt.subplot(131, projection=wcs)  
+plt.imshow(section1, origin='lower', cmap='Greys', vmin=median, vmax=median+5*std, interpolation='nearest') 
 
-""" plt.subplot(132, projection=wcs) 
-plt.imshow(hdu.data, origin='lower', cmap='Greys', vmin=mean, vmax=mean+5*std) 
-#plt.colorbar() """
+plt.subplot(132, projection=wcs) 
+plt.imshow(section1, origin='lower', cmap='Greys', vmin=median, vmax=median+5*std, interpolation='nearest') 
+#plt.colorbar()
 
 #WORK IN PROCESS :)
 
-daofind = DAOStarFinder(fwhm=3.0, threshold=5*std)
-sources = daofind(hdu.data-median)
+#DAO techniek
+daofind = DAOStarFinder(fwhm=4.0, threshold=5*std)
+sources = daofind(section1-median)
 for col in sources.colnames:
     if col not in ('id','npix'):
         sources[col].info.format = '%.2f'
-sources.pprint(max_width=76)
+sources.pprint(max_width=200)
+positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
+apertures = CircularAperture(positions, r=25)
+apertures.plot(color='red', lw=2, alpha=0.5)
+
+#peak techniek
+plt.subplot(133, projection=wcs) 
+plt.imshow(section1, origin='lower', cmap='Greys', vmin=median, vmax=median+5*std, interpolation='nearest') 
+
+tbl = find_peaks(section1, median+5*std, box_size=11)
+tbl['peak_value'].info.format = '%.8g'  # for consistent table output
+print(len(tbl))  # print only the first 10 peaks
+positions2 = np.transpose((tbl['x_peak'], tbl['y_peak']))
+apertures2 = CircularAperture(positions2, r=25)
+apertures2.plot(color='red', lw=2, alpha=0.5)
+
+
 plt.show()
 
