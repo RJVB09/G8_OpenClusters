1311185bf6d7bef3cdec0c60530db7252c18d970
toto1105 <ttbjones2004@gmail.com>
2024-06-06 23:50:36 +0200

    GEVONDEN STERREN IN 0306

--- code/star_finder.py
+++ code/star_finder.py
@@ -38,7 +38,7 @@ from photutils.aperture import CircularAperture
 from photutils.detection import find_peaks
 
 #zet de locatie van member data hier (het zit in hetzelfde mapje dus hoeft niet verandert te worden):
-image_file = get_pkg_data_filename('test.fits')
+image_file = get_pkg_data_filename('20240306_data.fits')
 hdu = fits.open(image_file)[0]
 wcs = WCS(hdu.header)
 mean, median, std = sigma_clipped_stats(hdu.data, sigma=3.0)
@@ -46,42 +46,48 @@ print('full img stats', mean, median, std)
 
 #hiermee kan je een deel van de data nemen ipv de hele afbeelding
 section1 = hdu.data[0:3900,35:4500]
-mask1 = hdu.data[1670:1850,2445:2645]
-mask2 = hdu.data[1800:2050,2730:2975]
-mask3 = hdu.data[2180:2320,3960:4080]
-
-plt.subplot(131)  
-plt.imshow(mask3, origin='lower', cmap='Greys', vmin=median, vmax=median+5*std, interpolation='nearest') 
-
-plt.subplot(132, projection=wcs) 
-plt.imshow(section1, origin='lower', cmap='Greys', vmin=median, vmax=median+5*std, interpolation='nearest') 
-#plt.colorbar()
-
-#WORK IN PROCESS :)
-
-#DAO techniek
-daofind = DAOStarFinder(fwhm=4.0, threshold=5*std)
-sources = daofind(section1-median)
-for col in sources.colnames:
-    if col not in ('id','npix'):
-        sources[col].info.format = '%.2f'
-sources.pprint(max_width=200)
-positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
-apertures = CircularAperture(positions, r=25)
-apertures.plot(color='red', lw=2, alpha=0.5)
+section2 = hdu.data[1700:2400,2250:4500]
 
 #peak techniek
-plt.subplot(133) 
-mask = np.zeros(data.shape, dtype=bool)
+#projection=wcs
+plt.subplot(121) 
+mask = np.zeros(section1.shape, dtype=bool)
+mask[1620:1850,2425:2630] = True
+mask[1800:2050,2730:2975] = True
+mask[2180:2320,3920:4080] = True
+mask[2020:2070,2685:2700] = True #ruis
+mask[965:990,1780:1808] = True
 plt.imshow(section1, origin='lower', cmap='Greys', vmin=median, vmax=median+5*std, interpolation='nearest') 
+tbl = find_peaks(section1, median+5*std, box_size=11, mask=mask, border_width=10, wcs=wcs)
 
-tbl = find_peaks(section1, median+5*std, box_size=11)
-tbl['peak_value'].info.format = '%.8g'  # for consistent table output
+tbl['peak_value'].info.format = '%.8g'
+
+
 print(len(tbl))  # print only the first 10 peaks
-positions2 = np.transpose((tbl['x_peak'], tbl['y_peak']))
+tbl['id'] = np.arange(len(tbl))
+positions = np.transpose((tbl['x_peak'], tbl['y_peak']))
+apertures = CircularAperture(positions, r=25)
+apertures.plot(color='red', lw=2, alpha=0.5)
+print(tbl)
+
+#Dit deel voegt de datapunten toe van sterren die uitgemask waren in het eerste gedeelte
+plt.subplot(122) 
+plt.imshow(section1, origin='lower', cmap='Greys', vmin=median, vmax=median+5*std, interpolation='nearest') 
+mask2 = np.zeros(section1.shape, dtype=bool)
+mask2[35:4500,0:2000] = True
+tbl2 = find_peaks(section1, median+5*std, box_size=50, border_width=10, wcs=wcs, npeaks=3, mask=mask2)
+positions2 = np.transpose((tbl2['x_peak'], tbl2['y_peak']))
+
 apertures2 = CircularAperture(positions2, r=25)
 apertures2.plot(color='red', lw=2, alpha=0.5)
 
+#data formatten
+tbl2['id'] = np.arange(len(tbl2))+len(tbl)
+for id in range(len(tbl2)):
+    tbl.add_row(tbl2[id])
 
+
+#tbl.write('stars_full_img.ecsv', overwrite=True) #file maken met data
 plt.show()
 
+#deel 3 :) DIT IS VOOR DE ANDERE AFBEELDING!
