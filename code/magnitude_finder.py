f671f745b2459ff6f08257119a5d7df1c2bc1477
RJVB09 <rjvb09@ziggo.nl>
2024-06-06 15:20:15 +0200

    Prepping stuff

--- code/magnitude_finder.py
+++ code/magnitude_finder.py
@@ -12,4 +12,12 @@ from photutils.detection import DAOStarFinder
 from photutils.aperture import CircularAperture
 from photutils.detection import find_peaks
 
-star 
+loc = Path(__file__).resolve().parent.parent
+loc = PurePath(loc,"data\\asu.tsv")
+print(loc)
+
+star_pos = pd.read_csv(loc, sep=';', comment='#')
+print(star_pos)
+
+for index, row in star_pos.iterrows():
+    print(row['c1'], row['c2'])
